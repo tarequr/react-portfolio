c5ae3da608c4274e1f3ea2392c16f087874186cb
tarequr <tarequrr8@gmail.com>
2024-05-21 22:19:53 +0600

    Create basic navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import style from './App.module.css'
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className={style.App}>
-      Hello World
+    <div className={styles.App}>
+      SAbbir
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
