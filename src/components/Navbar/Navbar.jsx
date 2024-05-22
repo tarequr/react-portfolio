aaf45fb88f085143b3bff7b8f19afb94fc759895
tarequr <tarequrr8@gmail.com>
2024-05-22 08:44:06 +0600

    Add Navbar component
    - Create Navbar component in src/components/Navbar.jsx
    - Add styling for Navbar in src/components/Navbar.css
    - Include Navbar in the main application in src/App.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import styles from './Navbar.module.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">Portfolio</a>
+
+      <div className={styles.menu}>
+        <ul className={styles.menuItmes}>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Exprience</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
