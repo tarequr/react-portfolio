194dd3fa6502c033a80a67cccef3922cb74ae4fb
tarequr <tarequrr8@gmail.com>
2024-05-22 15:17:11 +0600

    Add conditional rendering for menu button icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import styles from './Navbar.module.css'
+import { getImageUrl } from '../../utils'
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">Portfolio</a>
 
       <div className={styles.menu}>
+        <img className={styles.menuBtn} src={
+          menuOpen 
+          ? getImageUrl('nav/closeIcon.png') : getImageUrl('nav/menuIcon.png') 
+        } 
+        alt="menu-button" 
+        onClick={() => setMenuOpen(!menuOpen)}/>
         <ul className={styles.menuItmes}>
           <li>
             <a href="#">About</a>
