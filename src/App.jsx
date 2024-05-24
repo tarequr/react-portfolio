28dcb5460e125594802da82072fef7ee191c1098
tarequr <tarequrr8@gmail.com>
2024-05-24 20:32:45 +0600

    Add About section to the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import styles from "./App.module.css";
-import Hero from "./components/Hero/Hero";
-import Navbar from "./components/Navbar/Navbar";
+import { About } from "./components/About/About";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
-      <Hero/>
+      <Hero />
+      <About />
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React from "react";
 
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
-function Hero() {
+export const Hero = () => {
   return (
     <section className={styles.container}>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Hi, I am Sabbir</h1>
-            <p className={styles.description}>I'm a full-stack web developer with 3 years of experience React and Laravel framework. React out if you'd like to learn more!</p>
-            <a href="mailto:tarequrrahman@example.com" className={styles.contactBtn}>Contact Me</a>
-        </div>
-        <img src={getImageUrl('hero/heroImage.png')}  alt="Hero image of me" className={styles.heroImg} />
-        <div className={styles.topBlur}/>
-        <div className={styles.bottomBlur}/>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 5 years of experience using React and
+          NodeJS. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
     </section>
-  )
-}
-
-export default Hero
+  );
+};
