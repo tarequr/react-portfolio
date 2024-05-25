a976df173f0e0c38aa93389f337259eb3896c36d
tarequr <tarequrr8@gmail.com>
2024-05-25 17:13:38 +0600

    Add Experience section to the homepage
    - Create Experience component in src/components/Experience.jsx
    - Add styling for Experience component in src/components/Experience.css
    - Include Experience component in the main application in src/App.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
 
+import styles from './Experience.module.css';
+import skills from '../../data/skills.json';
+import histories from '../../data/history.json';
+import { getImageUrl } from '../../utils';
+
 function Experience() {
+    // console.log(skills);
   return (
-    <div>Experience</div>
+    <section className={styles.container} id="experience">
+        <h2 className={styles.title}>Experience</h2>
+        <div className={styles.content}>
+            <div className={styles.skills}>
+                {
+                    skills.map((skill,id ) => {
+                        return (
+                            <div key={id} className={styles.skill}>
+                                <div className={styles.skillImageContainer}>
+                                    <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                                </div>
+                                <p>{skill.title}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <ul className={styles.history}>
+                {
+                    histories.map((history, id) => {
+                        return (
+                            <li key={id} className={styles.historyItem}>
+                                <img src={getImageUrl(history.imageSrc)} alt={`${history.organisation} logo`} />
+
+                                <div className={styles.historyItemDetails}>
+                                    <h3>{`${history.role}, ${history.organisation}`}</h3>
+                                    <p>{`${history.startDate} - ${history.endDate}`}</p>
+
+                                    <ul>
+                                        {
+                                            history.experiences.map((experience, id) => {
+                                                return (
+                                                    <li key={id}>{experience}</li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    </section>
   )
 }
 
