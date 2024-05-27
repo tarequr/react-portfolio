d097e496ac83b146cf5599503fff714a1f4d4d23
tarequr <tarequrr8@gmail.com>
2024-05-27 22:18:14 +0600

    Add Contact section to the homepage
    - Create Contact component in src/components/Contact.jsx
    - Add styling for Contact component in src/components/Contact.module.css
    - Include Contact component in the main application in src/App.jsx

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import styles from './Contact.module.css';
+import { getImageUrl } from '../../utils';
+
+function Contact() {
+  return (
+    <footer id='contact' className={styles.container}>
+        <div className={styles.text}>
+            <h2>Contact</h2>
+            <p>Feel free to reach out</p>
+        </div>
+
+        <ul>
+            <li>
+                <img src={getImageUrl("contact/emailIcon.png")} alt="Email Icon" />
+                <a href="mailto:myemail@email.com">myemail@email.com</a>
+            </li>
+            <li>
+                <img src={getImageUrl("contact/linkedinIcon.png")} alt="Linkedin Icon" />
+                <a href="https://bd.linkedin.com/in/tarequrrahmansabbir">linkedin.com</a>
+            </li>
+            <li>
+                <img src={getImageUrl("contact/githubIcon.png")} alt="Email Icon" />
+                <a href="https://github.com/tarequr">github.com</a>
+            </li>
+        </ul>
+    </footer>
+  )
+}
+
+export default Contact
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,7 +6,7 @@ import styles from './Projects.module.css';
 
 function Projects() {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} id="projects">
         <h2 className={styles.title}>Projects</h2>
 
         <div className={styles.projects}>
